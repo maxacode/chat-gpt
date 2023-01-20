c6de371aba9b50ed1bbc1a3707ab29a0988263fc
Maks Derevencha <maxderevencha@gmail.com>
2023-01-19 22:13:59 -0500

    Inital run

--- test.py
+++ test.py
@@ -2,5 +2,23 @@
 import os
 import openai
 openai.organization = "org-k3FtMeCql1X63YVJ9hrdjyXP"
-openai.api_key = os.getenv("OPENAI_API_KEY")
-print(openai.Model.list())
+openai.api_key_path = "key.txt"
+#with open("Sample-Outputs/Model-list-output.txt", "w") as file:
+  #  file.write(str(openai.Model.list()))
+    
+prompt = "What is AI"
+
+response = openai.Completion.create(
+  model="text-davinci-003",
+  prompt=prompt,
+  temperature=0.7,
+  max_tokens=256,
+  top_p=1,
+  frequency_penalty=0,
+  presence_penalty=0
+)
+print(response)
+
+with open("Sample-Outputs/text-davinci-what-is-ai.txt", "w") as file:
+   file.write(str(response))
+    
